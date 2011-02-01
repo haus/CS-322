46010bd529f452e83a5b44e834455d6f9f3c1b89
Matthaus Litteken <mlitteken@gmail.com>
2011-02-01 01:12:53 -0800

    Cleaning assembly again.

--- hw2/qs_ptr.s
+++ hw2/qs_ptr.s
@@ -1,11 +1,6 @@
-	.file	"qs_ptr.c"
-	.text
-	.p2align 4,,15
 .globl partition
 	.type	partition, @function
 partition:
-.LFB0:
-	.cfi_startproc
 	movq	(%rsi), %rax
 	movsd	(%rdi), %xmm1
 	cmpq	%rsi, %rdi
@@ -13,8 +8,6 @@ partition:
 	movsd	%xmm1, (%rsi)
 	movq	%rdi, %rax
 	jae	.L3
-	.p2align 4,,10
-	.p2align 3
 .L9:
 	movsd	(%rdi), %xmm0
 	ucomisd	%xmm0, %xmm1
@@ -33,28 +26,15 @@ partition:
 	movsd	%xmm1, (%rax)
 	movq	%rdx, (%rsi)
 	ret
-	.cfi_endproc
-.LFE0:
-	.size	partition, .-partition
-	.p2align 4,,15
 .globl quicksort
 	.type	quicksort, @function
 quicksort:
-.LFB1:
-	.cfi_startproc
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
 	movq	%rsi, %rbp
-	.cfi_offset 6, -16
 	pushq	%rbx
-	.cfi_def_cfa_offset 24
 	subq	$8, %rsp
-	.cfi_def_cfa_offset 32
 	cmpq	%rdi, %rsi
 	jbe	.L20
-	.cfi_offset 3, -24
-	.p2align 4,,10
-	.p2align 3
 .L22:
 	movq	(%rbp), %rax
 	movsd	(%rdi), %xmm1
@@ -64,8 +44,6 @@ quicksort:
 	movsd	%xmm1, (%rbp)
 	movq	%rdi, %rax
 	jbe	.L15
-	.p2align 4,,10
-	.p2align 3
 .L23:
 	movsd	(%rax), %xmm0
 	ucomisd	%xmm0, %xmm1
@@ -93,22 +71,11 @@ quicksort:
 	popq	%rbx
 	popq	%rbp
 	ret
-	.cfi_endproc
-.LFE1:
-	.size	quicksort, .-quicksort
-	.p2align 4,,15
 .globl qs
 	.type	qs, @function
 qs:
-.LFB2:
-	.cfi_startproc
 	movl	%edi, %eax
 	movq	%rsi, %rdi
 	cltq
 	leaq	-8(%rsi,%rax,8), %rsi
 	jmp	quicksort
-	.cfi_endproc
-.LFE2:
-	.size	qs, .-qs
-	.ident	"GCC: (Ubuntu/Linaro 4.4.4-14ubuntu5) 4.4.5"
-	.section	.note.GNU-stack,"",@progbits
