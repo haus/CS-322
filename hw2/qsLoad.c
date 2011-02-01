48c231012cd0e4a1030fd6781e4cdb2f16129f86
Matthaus Litteken <mlitteken@gmail.com>
2011-01-31 19:19:21 -0800

    Added some prettiness...

--- hw2/qsLoad.c
+++ hw2/qsLoad.c
@@ -8,21 +8,29 @@ int main(int argc, char** argv) {
 	if (argc == 2) {
 		arraySize = strtoul(argv[1], NULL, 10);
 	} else {
-		arraySize = 400000;
+		arraySize = 40;
 	}
 	
 	double* array = (double *) malloc(sizeof(double) * arraySize);
 	srand(time(0));	
 
+	printf("Unsorted array.\n");
 	for (int i = 0; i < arraySize; i++) {
 		array[i] = ((double)rand()/(RAND_MAX)+1);
-		printf("%f, ", array[i]);
+		if (i == arraySize - 1)
+			printf("%f\n", array[i]);
+		else
+			printf("%f, ", array[i]);
 	}
 	
 	qs(arraySize, array);
 
+	printf("Sorted array.\n");
 	for (int i = 0; i < arraySize; i++) {
-		printf("%f, ", array[i]);
+		if (i == arraySize - 1)
+			printf("%f\n", array[i]);
+		else
+			printf("%f, ", array[i]);
 	}
 
 	return 0;
